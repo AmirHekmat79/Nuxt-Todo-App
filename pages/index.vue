c028f30480db6052c2e13f32620b235bea4e640d
AmirHekmat79 <amir.hekmat1379@gmail.com>
2022-07-19 14:41:31 +0430

    Make Todo Page Structure

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,17 @@
-<template></template>
-
+<template>
+  <div>
+    <div class="container">
+      <span>
+        <font-awesome-icon icon="moon"> </font-awesome-icon>
+      </span>
+      <h1>To Do List App</h1>
+      <div class="addToolsContainer">
+        <input type="text" class="toDoInput" placeholder="Add Your New ToDo" />
+        <button class="addButton">Add</button>
+      </div>
+    </div>
+  </div>
+</template>
 <script lang="ts">
 import Vue from "vue";
 
