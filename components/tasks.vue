efb2eda912fb1755fa5ab121105698aa8e755a79
AmirHekmat79 <amir.hekmat1379@gmail.com>
2022-07-19 20:50:28 +0430

    Make tasks Component

---
+++ components/tasks.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import { tasks, ITodo } from "../scripts/Type";
+export default Vue.extend({
+  name: "tasks",
+  data() {
+    return {};
+  },
+  methods: {},
+});
+</script>
+<style scoped></style>
