df3c60b4aeafb78ab5bcd0cdee82a0b7662b8823
AmirHekmat79 <amir.hekmat1379@gmail.com>
2022-07-19 14:45:45 +0430

    Add Styles To Todo Page Structure

--- pages/index.vue
+++ pages/index.vue
@@ -19,3 +19,34 @@ export default Vue.extend({
   name: "IndexPage",
 });
 </script>
+<style scoped>
+.container h1 {
+  text-align: center;
+  margin: 30px;
+}
+.addToolsContainer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.addToolsContainer .addButton,
+.toDoInput {
+  margin: 5px;
+  padding: 10px 20px;
+  border-radius: 10px;
+}
+
+.toDoInput {
+  width: 100%;
+  text-align: center;
+}
+.addToolsContainer .addButton {
+  background: linear-gradient(#ffb347, #ffcc33);
+}
+
+.addToolsContainer .addButton:hover {
+  background: linear-gradient(#66ff47, #33ff81);
+  cursor: pointer;
+}
+</style>
